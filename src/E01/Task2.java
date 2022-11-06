0dc9876862be2ae1d997e5474a66b2d76a9e6047
Jakop <j.ppp32123@gmail.com>
2022-11-06 22:27:09 +0100

    Task2 from E01 done.

--- src/E01/Task2.java
+++ src/E01/Task2.java
@@ -2,6 +2,12 @@ package E01;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        System.out.println("Only A: A");
+        System.out.print("A+0: ");
+        System.out.print('A'+1);
     }
 }
+/*
+* Adding a value to a string turn's it into it's decimal form.
+* A = 65 in unicode
+*/
