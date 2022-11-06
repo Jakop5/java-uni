98c0599c4551492fefd4c31a39a2aa83eeb58a1b
Jakop <j.ppp32123@gmail.com>
2022-11-06 22:10:28 +0100

    changed the project structure. done task1 from E01.

---
+++ src/E01/Task1.java
@@ -0,0 +1,13 @@
+package E01;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 121, b = 0171, c = 0b1111001, d = 0x79 ;
+            // decimal, octal , binary , hex
+
+        System.out.println("Decimal -> " + a);
+        System.out.println("Octal -> " + b);
+        System.out.println("Binary -> " + c);
+        System.out.println("Hexadecimal -> " + d);
+    }
+}
