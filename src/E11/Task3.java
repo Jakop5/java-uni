f720d6fac0706b82c701b2b08ea6ec4a18188e3f
Jakop <j.ppp32123@gmail.com>
2022-12-01 12:41:23 +0100

    e11/e12 added

---
+++ src/E11/Task3.java
@@ -0,0 +1,9 @@
+package E11;
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args){
+        System.out.println(FuncStat.fiboR(45));
+        System.out.println(FuncStat.fiboI(45));
+    }
+}
